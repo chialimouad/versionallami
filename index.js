2ec78d49c168b5a049de3196cdb934f7651dfe99
chialimouad <chialimouaduae@gmail.com>
2024-02-21 22:13:44 +0100

    new

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express =require('express')
 const bodyparser =require('body-parser')
-
+const cors =  require('cors')
 const useroute =require('./routes/routes')
 const app = express()
 app.use(bodyparser.json())
@@ -8,5 +8,7 @@ app.use(bodyparser.raw())
 app.use(bodyparser.text())
 app.use(bodyparser.urlencoded({extended:true}))
 app.use('/',useroute)
-
+app.use(cors({origin:'mongodb+srv://chialimouad:mouadchiali@cluster0.z6qhktx.mongodb.net/test',
+credentials:true,            //access-control-allow-credentials:true
+optionSuccessStatus:200}))
 module.exports=app
