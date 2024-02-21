3343090ca00ae736f3f0b7919caeb840e0cbd856
chialimouad <chialimouaduae@gmail.com>
2024-02-21 21:42:40 +0100

    new

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyparser.raw())
 app.use(bodyparser.text())
 app.use(bodyparser.urlencoded({extended:true}))
 app.use('/',useroute)
-app.use(cors({origin:'https://versionallami.onrender.com/register',
+app.use(cors({
 credentials:true,            //access-control-allow-credentials:true
 optionSuccessStatus:200}))
 module.exports=app
