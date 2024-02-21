dc3a7a31fa381f46c7465ed693697a288deb3de7
chialimouad <chialimouaduae@gmail.com>
2024-02-21 23:06:44 +0100

    new

--- controller/controller.js
+++ controller/controller.js
@@ -1,10 +1,12 @@
+const { default: axios } = require('axios')
 const userserv =require('../services/services')
 
 
 exports.register= async(req,res,next)=>{try{
     const {email,fullname,phonenumber,idnumber,desc,Skills,grade}=req.body
     const usercontrol =await userserv.registeruser(email,fullname,phonenumber,idnumber,desc,Skills,grade)
-
+       axios.post('https://versionallami.onrender.com/register')
     res.json({status:true,success:"user succsefully"})
+    
 }catch(err){console.log(err)}}
 
