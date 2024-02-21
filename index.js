d09e2aac4656c01e0e059c9c756aae6f80250a5a
chialimouad <chialimouaduae@gmail.com>
2024-02-21 23:52:41 +0100

    new

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const cors =  require('cors')
 const useroute =require('./routes/routes')
 const app = express()
 app.use(bodyparser.json())
-
 app.use(bodyparser.urlencoded({extended:true}))
 app.use('/',useroute)
-app.use(cors())
+app.use(cors({origin:"*",}))
 module.exports=app
