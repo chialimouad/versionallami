709efe7900c9537cd9f28223663d52b2cc5d567c
chialimouad <chialimouaduae@gmail.com>
2024-02-21 21:18:26 +0100

    cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express =require('express')
 const bodyparser =require('body-parser')
+const cors =  require('cors')
 const useroute =require('./routes/routes')
 const app = express()
 app.use(bodyparser.json())
@@ -7,5 +8,5 @@ app.use(bodyparser.raw())
 app.use(bodyparser.text())
 app.use(bodyparser.urlencoded({extended:true}))
 app.use('/',useroute)
-
+app.use(cors())
 module.exports=app
