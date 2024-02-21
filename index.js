5aa46aaf59a2836ea5b13e152e92cb99302ce963
chialimouad <chialimouaduae@gmail.com>
2024-02-21 22:01:58 +0100

    new

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express =require('express')
 const bodyparser =require('body-parser')
+const cors =  require('cors')
 const useroute =require('./routes/routes')
 const app = express()
 app.use(bodyparser.json())
@@ -7,5 +8,7 @@ app.use(bodyparser.raw())
 app.use(bodyparser.text())
 app.use(bodyparser.urlencoded({extended:true}))
 app.use('/',useroute)
-
+app.use(cors({origin:'http://localhost:5173',
+credentials:true,            //access-control-allow-credentials:true
+optionSuccessStatus:200}))
 module.exports=app
