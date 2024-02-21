73d8721a781084ab0fd7268eafeebc69b00ad086
chialimouad <chialimouaduae@gmail.com>
2024-02-21 21:45:19 +0100

    new

--- index.js
+++ index.js
@@ -8,7 +8,5 @@ app.use(bodyparser.raw())
 app.use(bodyparser.text())
 app.use(bodyparser.urlencoded({extended:true}))
 app.use('/',useroute)
-app.use(cors({
-credentials:true,            //access-control-allow-credentials:true
-optionSuccessStatus:200}))
+app.use(cors())
 module.exports=app
