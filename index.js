f02b4d2695e6a96d15e5bb0007ff8d191c50166b
chialimouad <chialimouaduae@gmail.com>
2024-02-22 21:40:08 +0100

    NOO

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express =require('express')
 const bodyparser =require('body-parser')
-const cors =  require('cors')
+
 const useroute =require('./routes/routes')
 const app = express()
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({extended:true}))
 app.use('/',useroute)
-app.use(cors({origin:"https://versionallami.onrender.com/register    ",}))
+
 module.exports=app
