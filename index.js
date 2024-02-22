acef215e66893d474bfd9ec057eb1a6c75551a66
chialimouad <chialimouaduae@gmail.com>
2024-02-22 22:08:06 +0100

    newww

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors =  require('cors')
 const useroute =require('./routes/routes')
 const app = express()
 app.use(bodyparser.json())
-app.use(cors({origin:"https://k-linker-club.netlify.app",}))
+app.use(cors({origin:"*",}))
 app.use(bodyparser.urlencoded({extended:true}))
 app.use('/12',useroute)
 module.exports=app
