5258094e379b3b74a4a80dd239fb947ff2b4f41b
chialimouad <chialimouaduae@gmail.com>
2024-02-22 21:55:13 +0100

    new

--- controller/controller.js
+++ controller/controller.js
@@ -1,10 +1,10 @@
 const userserv =require('../services/services')
-const app =require('../index')
-const sayah =require('cors')
+
+
 exports.register= async(req,res,next)=>{try{
     const {email,fullname,phonenumber,idnumber,desc,Skills,grade}=req.body
     const usercontrol =await userserv.registeruser(email,fullname,phonenumber,idnumber,desc,Skills,grade)
-    app.sayah({origin:"*"})
+
     res.json({status:true,success:"user succsefully"})
     
 }catch(err){console.log(err)}}
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express =require('express')
 const bodyparser =require('body-parser')
-
+const cors =  require('cors')
 const useroute =require('./routes/routes')
 const app = express()
 app.use(bodyparser.json())
+app.use(cors({origin:"*",}))
 app.use(bodyparser.urlencoded({extended:true}))
-app.use('/',useroute)
-
+app.use('/12',useroute)
 module.exports=app
